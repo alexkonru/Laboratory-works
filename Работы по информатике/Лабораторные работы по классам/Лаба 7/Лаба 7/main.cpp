a56fc70cc43d37a1e4bd62a759f20bb2e886c74b
alexkonru <Skonuaev595@yandex.ru>
2023-04-30 22:29:13 +0500

    Лаба 7 готовая

--- Работы по информатике/Лабораторные работы по классам/Лаба 7/Лаба 7/main.cpp
+++ Работы по информатике/Лабораторные работы по классам/Лаба 7/Лаба 7/main.cpp
@@ -1,14 +1,35 @@
 #include "LinkedList.h"
-#include "Time.h"
-#include "node.h"
+#include "Pair.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	LinkedList<int>A(5, 0);
+	LinkedList<int>A(5,0);
 	cin >> A;
-	cout << A;
+	cout << A << endl;
+	LinkedList<int>B(10, 1);
+	cout << B << endl;
+	B = A;
+	cout << B << endl;
+	cout << A[2] << endl;
+	cout << A + B;
+	B = A + 10;
+	cout << B;
+	Pair p;
+	cin >> p;
+	cout << p;
+	LinkedList<Pair>C(5, p);
+	cin >> C;
+	cout << C << endl;
+	LinkedList<Pair>D(2, p);
+	cout << D << endl;
+	D = C;
+	cout << D << endl;
+	cout << C[2] << endl;
+	cout << C + D << endl;
+	D = C + p;
+	cout << D << endl;
 	return 0;
 }
--- Работы по информатике/Лабораторные работы по классам/Лаба 7/Лаба 7/node.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-template<class T>
-class Node
-{
-public:
-    T data;
-    Node<T>* next = nullptr;
-    Node<T>* prev = nullptr;
-};
