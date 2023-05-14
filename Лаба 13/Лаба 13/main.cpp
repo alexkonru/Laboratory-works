03c87983c87f135c65918c766d7c2bade4b2990d
alexkonru <116183825+alexkonru@users.noreply.github.com>
2023-05-14 16:23:37 +0500

    Add files via upload

---
+++ Лаба 13/Лаба 13/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "z1.h"
+#include "z2.h"
+#include "z3.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int n;
+    cout << "Какую задачу нужно решить? (1-3)\n";
+    cin >> n;
+    switch (n)
+    {
+    case 1: z1 Z1 = z1();
+        Z1.main1();
+        break;
+    case 2: z2 Z2 = z2();
+        Z2.main();
+        break;
+    case 3: z3 Z3 = z3();
+        Z3.main();
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
---
+++ Лаба 13/Лаба 13/z1.h
@@ -0,0 +1,92 @@
+#pragma once
+#include <iostream>
+#include <deque>
+#include <algorithm>
+#include <numeric>
+#include "Pair.h"
+using namespace std;
+
+typedef deque<Pair>TDec;
+class z1
+{
+public:
+    TDec make(double n)
+    {
+        TDec s;
+        Pair a;
+        for (double i = 0; i < n; i++)
+        {
+            cin >> a;
+            s.push_back(a);
+        }
+        return s;
+    }
+    void print(TDec s)
+    {
+        if (s.size() > 0) {
+            for_each(s.begin(), s.end(), [](Pair p) {
+                cout << p << '\n';
+                });
+        }
+        else {
+            cout << "Список пуст!";
+        }
+        cout << '\n';
+    }
+    Pair max(TDec s)
+    {
+        Pair m = *max_element(s.begin(), s.end());
+        return m;
+    }
+
+    TDec push_back_list(TDec s, Pair el)
+    {
+        s.push_back(el);
+        return s;
+    }
+
+
+    TDec pop_el(TDec st, Pair f, Pair s)
+    {
+        auto i = remove_if(st.begin(), st.end(), [f, s](Pair p) {
+            return ((f.first <= p.get_first() && p.get_first() <= s.first) || (f.second <= p.get_second() && p.get_second() <= s.second));
+            });
+        st.erase(i, st.end());
+        return st;
+    }
+
+    Pair sred(TDec st)
+    {
+        Pair Middle = accumulate(st.begin(), st.end(), Pair(0, 0));
+        return Middle;
+    }
+
+    TDec plus_const(TDec st, Pair s)
+    {
+        for (Pair& c : st) {
+            c + s;
+        }
+        return st;
+    }
+
+    int main1()
+    {
+        TDec st;
+       /* int n;
+        cout << "n = "; cin >> n;
+        st = make(n);
+        print(st);
+        Pair el = max(st);
+        st = push_back_list(st, el);
+        print(st);
+        Pair f, s;
+        cin >> f;
+        cin >> s;
+        st = pop_el(st, f, s);
+        print(st);
+        Pair sr = sred(st);
+        st = plus_const(st, sr);
+        print(st);*/
+        return 0;
+    }
+};
